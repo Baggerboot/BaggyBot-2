01a85447386da1943f14a14a3bbc4ffc3e33ad2f
Baggykiin <jhgeluk@gmail.com>
2014-03-04 10:43:13 +0100

    Allow disabling of oidentd integration
    Additionall, add a variable in the settings file for the location of the
    .oidentd.conf file

--- BaggyBot 2.0/DataProcessors/IO/IdentWriter.cs
+++ BaggyBot 2.0/DataProcessors/IO/IdentWriter.cs
@@ -10,13 +10,19 @@ namespace BaggyBot.DataProcessors
 {
 	class IdentWriter
 	{
-		private const string identfile = @"/home/baggybotuser/.oidentd.conf";
+		private string identfile = @"/home/baggybotuser/.oidentd.conf";
 
 		public void Write(int port, string ident)
 		{
-			Logger.Log("Writing ident '{0}' and port '{1}' to identfile", LogLevel.Debug, true, ident, port);
-			using (StreamWriter sw = new StreamWriter(new FileStream(identfile, FileMode.Create, FileAccess.ReadWrite))) {
-				sw.WriteLine("lport " + port + " { reply \"" + ident + "\" }");
+			if (Settings.Instance["enable_oidentd"] == "true") {
+				var settingsIdentFile = Settings.Instance["identfile_location"];
+				if (settingsIdentFile != string.Empty) {
+					identfile = settingsIdentFile;
+				}
+				Logger.Log("Writing ident '{0}' and port '{1}' to identfile", LogLevel.Debug, true, ident, port);
+				using (StreamWriter sw = new StreamWriter(new FileStream(identfile, FileMode.Create, FileAccess.ReadWrite))) {
+					sw.WriteLine("lport " + port + " { reply \"" + ident + "\" }");
+				}
 			}
 		}
 	}
