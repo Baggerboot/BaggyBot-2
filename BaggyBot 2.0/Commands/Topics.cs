25c58b190d336f48892907987adc2f6661b8ad9c
Baggykiin <jhgeluk@gmail.com>
2014-03-13 12:14:20 +0100

    Add topic & wiki command

---
+++ BaggyBot 2.0/Commands/Topics.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using BaggyBot.DataProcessors;
+
+namespace BaggyBot.Commands
+{
+	class Topics : ICommand
+	{
+		public PermissionLevel Permissions { get { return PermissionLevel.All; } }
+
+		private DataFunctionSet dataFunctionSet;
+
+		public Topics(DataFunctionSet df)
+		{
+			dataFunctionSet = df;
+		}
+
+		private void ShowTopics(string nick, string channel, Action<string, object[]> replyCallback, bool showDebugInfo)
+		{
+			Logger.Log("Showing topics for " + nick);
+			int userId = dataFunctionSet.GetIdFromNick(nick);
+			var topics = dataFunctionSet.FindTopics(userId, channel);
+
+			if (topics == null) {
+				replyCallback("Could not find any IRC data by {0}. Did you spell their name correctly?", new [] { nick });
+				return;
+			}
+
+			string topicString;
+
+			if (showDebugInfo) {
+				topicString = string.Join(", ", topics.Take(20).Select(pair => string.Format("\x02{0}\x02 ({1}/{2}: {3:N2})", pair.Name, pair.UserCount, pair.GlobalCount, pair.Score)));
+			} else {
+				topicString = string.Join(", ", topics.Take(20).Select(pair => pair.Name));
+			}
+
+			
+
+			replyCallback("Words associated with {0}: {1}", new [] {nick, topicString });
+		}
+
+		public void Use(CommandArgs command)
+		{
+			bool showDebugInfo = false;
+			if (command.Args[0] == "-d") {
+				command.Args = command.Args.Skip(1).ToArray();
+				showDebugInfo = true;
+			}
+			if (command.Args.Length == 0) {
+				ShowTopics(command.Sender.Nick, command.Channel, command.Reply, showDebugInfo);
+			} else if (command.Args.Length > 2) {
+				command.ReturnMessage("Usage: -topics [nick]");
+			} else if(command.Args.Length == 2){
+				ShowTopics(command.Args[0], command.Args[1], command.Reply, showDebugInfo);
+			} else {
+				ShowTopics(command.Args[0], command.Channel, command.Reply, showDebugInfo);
+			}
+		}
+	}
+}
---
+++ BaggyBot 2.0/Commands/Wikipedia.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO;
+using System.Net;
+using Newtonsoft.Json.Linq;
+
+namespace BaggyBot.Commands
+{
+	class Wikipedia : ICommand
+	{
+		public PermissionLevel Permissions { get { return PermissionLevel.All; } }
+
+		public void Use(CommandArgs command)
+		{
+			var uri = new Uri(string.Format("http://en.wikipedia.org/w/api.php?format=json&action=query&titles={0}&prop=revisions&rvprop=content", command.FullArgument));
+
+			var rq = HttpWebRequest.Create(uri);
+			var response = rq.GetResponse();
+
+			using (StreamReader sr = new StreamReader(response.GetResponseStream())) {
+				dynamic jsonObj = JObject.Parse(sr.ReadToEnd());
+				Console.WriteLine("Title: " + jsonObj.query.pages[0].title);
+			}
+		}
+	}
+}
