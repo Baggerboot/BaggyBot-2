95d8e2e0fe16168e719acafd73367080c7a7004d
Baggykiin <jhgeluk@gmail.com>
2016-01-10 22:36:54 +0100

    Remove CPU information from uptime

--- BaggyBot 2.0/Commands/Uptime.cs
+++ BaggyBot 2.0/Commands/Uptime.cs
@@ -25,20 +25,7 @@ namespace BaggyBot.Commands
 			var h = diff.Hours + (diff.Hours == 1 ? " hour" : " hours");
 			var m = diff.Minutes + (diff.Minutes == 1 ? " minute" : " minutes");
 			var s = diff.Seconds + (diff.Seconds == 1 ? " second" : " seconds");
-
-			var pc = new PerformanceCounter("Processor", "% Processor Time", "_Total");
-			pc.NextValue();
-			Thread.Sleep(1000); // wait a second to get a valid reading
-
-			var sb = new StringBuilder();
-			sb.Append("CPU Usage: ");
-			foreach(var name in instances){
-				pc.InstanceName = name;
-				pc.CategoryName = "Processor Information";
-				sb.Append(string.Format("{0} - {1:f}", name, pc.NextSample()));
-			}
-
-			command.Reply("I have been running for {0}, {1}, {2} and {3} - {4}", d, h, m, s, sb.ToString());
+			command.Reply("I have been running for {0}, {1}, {2} and {3}", d, h, m, s);
 		}
 	}
 }
