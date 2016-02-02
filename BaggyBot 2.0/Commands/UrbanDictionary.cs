897c8ef632d88158f60578a8e1b6ef4a96e92ace
Baggykiin <baggykiin@jgeluk.net>
2016-02-02 13:21:00 +0100

    Re-enable UrbanDictionary command

--- BaggyBot 2.0/Commands/UrbanDictionary.cs
+++ BaggyBot 2.0/Commands/UrbanDictionary.cs
@@ -21,52 +21,35 @@ namespace BaggyBot.Commands
 
 			var term = command.FullArgument.Replace(' ', '+');
 
-			var rq = WebRequest.Create(@"https://www.kimonolabs.com/api/2rtoqq4w?apikey=rNsrJf1WHUHasu47YT5pxcr5anzCkji8&term=" + term);
+			var rq = WebRequest.Create(@"http://api.urbandictionary.com/v0/define?term=" + term);
 			var response = rq.GetResponse();
 			var text = new StreamReader(response.GetResponseStream()).ReadToEnd();
 			dynamic obj = JObject.Parse(text);
 
 			string name;
 			string definition;
-			string example;
+			string example = "";
 
-			name = (string)obj.results.top[0].name.text;
+			name = (string)obj.list[0].word;
 
 			try
 			{
-				definition = (string)obj.results.top[0].definition;
+				definition = (string)obj.list[0].definition;
 			}
 			catch (Exception)
 			{
-				try
-				{
-					definition = (string)obj.results.top[0].definition.text;
-				}
-				catch (Exception)
-				{
-					command.Reply("unable to find a definition for \"{0}\"", command.FullArgument);
-					return;
-				}
+				command.Reply("unable to find a definition for \"{0}\"", command.FullArgument);
+				return;
 			}
 
 			try
 			{
-				example = (string)obj.results.top[0].example;
+				example = (string)obj.list[0].example;
 			}
 			catch (Exception)
 			{
-				try
-				{
-					example = (string)obj.results.top[0].example.text;
-				}
-				catch (Exception)
-				{
-					command.Reply("unable to find a definition for \"{0}\"", command.FullArgument);
-					return;
-				}
+				// ignored
 			}
-
-
 
 			name = Regex.Replace(name, @"\t|\n|\r", " ");
 			definition = Regex.Replace(definition, @"\t|\n|\r", " ");
@@ -74,16 +57,18 @@ namespace BaggyBot.Commands
 
 			if (definition.Length > 255)
 			{
-				definition = definition.Substring(0, 255);
+				definition = definition.Substring(0, 250);
 				definition += " (...)";
 			}
 
 			if (example.Length > 255)
 			{
-				example = example.Substring(0, 255);
+				example = example.Substring(0, 250);
 				example += " (...)";
 			}
-			command.ReturnMessage("\u0002{0}\u0002: {1} - \u001d{2}\u001d - http://urbandictionary.com/define.php?term={3}", name, definition, example, term);
+			var exampleString = string.IsNullOrWhiteSpace(example) ? "" : $" - \u001d{example}\u001d";
+
+			command.ReturnMessage("\u0002{0}\u0002: {1}{2} - http://urbandictionary.com/define.php?term={3}", name, definition, exampleString, term);
 		}
 	}
 }
