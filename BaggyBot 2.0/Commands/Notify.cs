247312de4d320c6f21fcd54e954e683b669286c8
Baggykiin <baggykiin@jgeluk.net>
2016-02-05 17:13:46 +0100

    Add a notification 'command'

---
+++ BaggyBot 2.0/Commands/Notify.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaggyBot.Commands
+{
+	class Notify : ICommand
+	{
+		public PermissionLevel Permissions { get { return PermissionLevel.All; } }
+		private string message;
+		public Notify(string message)
+		{
+			this.message = message;
+		}
+
+		public void Use(CommandArgs command)
+		{
+			command.Reply(message);
+		}
+	}
+}
