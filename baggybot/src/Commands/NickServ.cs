8b7535872b7d9e58ed618c7ed7c0b98114a88bea
Baggykiin <baggykiin@jgeluk.net>
2016-02-15 14:35:41 +0100

    Allow users to update their own Nickserv accountname

--- baggybot/src/Commands/NickServ.cs
+++ baggybot/src/Commands/NickServ.cs
@@ -6,12 +6,13 @@ namespace BaggyBot.Commands
 	internal class NickServ : Command
 	{
 		public override PermissionLevel Permissions => PermissionLevel.All;
-		public override string Usage => "[-f]|[set <username>]";
+		public override string Usage => "[-f]|[update]|[set <username>]";
 		public override string Description => "Performs a NickServ lookup for your username against the database. Use `-f` to query the NickServ service instead, or use `set` to store a user's current NickServ username in the database.";
 		
+		// TODO: Candidate for porting to CommandParsing
 		public override void Use(CommandArgs command)
 		{
-			if (command.Args.Length == 2 && command.Args[0].Equals("set"))
+			if (command.Args.Length == 2 && command.Args[0] == "set")
 			{
 				if (UserTools.Validate(command.Sender))
 				{
@@ -26,7 +27,16 @@ namespace BaggyBot.Commands
 				command.ReturnMessage(Messages.CmdNotAuthorised);
 				return;
 			}
-			if (command.Args.Length == 1 && command.Args[0].Equals("-f"))
+			if(command.Args.Length == 1 && command.Args[0] == "update")
+			{
+				var uid = command.Client.StatsDatabase.GetIdFromNick(command.Sender.Nick);
+				if (uid < 0) uid = command.Client.StatsDatabase.GetIdFromUser(command.Sender);
+				var nickserv = command.Client.NickservLookup(command.Sender.Nick);
+				var rows = command.Client.StatsDatabase.SetNsLogin(uid, nickserv?.AccountName);
+				command.Reply($"NickServ reports that your account name is '{nickserv?.AccountName}'. I've updated the database to reflect that ({rows} record(s) affected).");
+                return;
+			}
+			if (command.Args.Length == 1 && command.Args[0] == "-f")
 			{
 				command.ReturnMessage("Sending a NickServ call");
 				var username = command.Client.NickservLookup(command.Sender.Nick);
@@ -39,7 +49,7 @@ namespace BaggyBot.Commands
 					return;
 				}
 			}
-			else if (command.Args.Length != 0)
+			if (command.Args.Length != 0)
 			{
 				command.Reply("Usage: -ns; -ns add <username>");
 				return;
@@ -47,7 +57,7 @@ namespace BaggyBot.Commands
 			var ns = command.Client.StatsDatabase.GetNickserv(command.Client.StatsDatabase.GetIdFromUser(command.Sender));
 			if (ns == null)
 			{
-				command.Reply("you don't appear to be identified with NickServ.");
+				command.Reply("According to my database, you don't use NickServ. If that's not right, try running -ns update");
 			}
 			else {
 				command.Reply("your NickServ username is " + ns);
