902a378b6f5e49ee4e7979ba79475a30bbbb5e2c
Johan Geluk <jhgeluk@gmail.com>
2016-02-16 02:49:29 +0100

    Add GitHub link

--- baggybot/src/EmbeddedData/Messages.cs
+++ baggybot/src/EmbeddedData/Messages.cs
@@ -3,6 +3,6 @@
 	public class Messages
 	{
 		public const string CmdNotAuthorised = "You are not authorised to use this command.";
-		public const string CmdGeneralInfo = "BaggyBot {0} -- Stats page: http://www.jgeluk.net/stats -- Made by baggerboot. For help, try the -help command.";
+		public const string CmdGeneralInfo = "BaggyBot {0} -- Stats page: http://www.jgeluk.net/stats -- Source code: https://github.com/Baggykiin/BaggyBot-2 -- Made by baggerboot. For help, try the -help command.";
 	}
 }
