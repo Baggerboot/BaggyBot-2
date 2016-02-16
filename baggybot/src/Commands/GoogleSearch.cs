62e85f0017598d926df217b1cb6b1151ddae0de4
Baggykiin <baggykiin@jgeluk.net>
2016-02-16 15:07:41 +0100

    Add usage information

--- baggybot/src/Commands/GoogleSearch.cs
+++ baggybot/src/Commands/GoogleSearch.cs
@@ -14,8 +14,8 @@ namespace BaggyBot.Commands
 	internal class GoogleSearch : Command
 	{
 		public override PermissionLevel Permissions { get { return PermissionLevel.All; } }
-		public override string Usage => "";
-		public override string Description => "";
+		public override string Usage => "<search term>";
+		public override string Description => "Searches Google for the given term, and returns the first result.";
 
 		public override void Use(CommandArgs command)
 		{
