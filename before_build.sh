9a0073c1dd4830a2b4330443d6fbf2a589d77d44
Johan Geluk <jhgeluk@gmail.com>
2017-02-08 22:33:15 +0100

    Update and rename build.sh to before_build.sh

--- before_build.sh
+++ before_build.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
 echo "namespace BaggyBot{public sealed partial class Bot{public static string Version =>\""`git describe --tags`"\";}}" > ./baggybot/src/Version.cs
-
-xbuild /p:Configuration=Release baggybot.sln
