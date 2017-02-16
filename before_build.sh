ef6f26250436bc6bf7f9eea84fbe65d96bfa9727
Johan Geluk <jhgeluk@gmail.com>
2017-02-16 21:39:05 +0100

    Update before_build.sh

--- before_build.sh
+++ before_build.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-git submodule update --init
+
+cd SlackAPI
+nuget restore
+cd ..
+
 echo "namespace BaggyBot{public sealed partial class Bot{public static string Version =>\""`git describe --tags`"\";}}" > ./baggybot/src/Version.cs
