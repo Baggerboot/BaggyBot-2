decbe760270a12492d852836cd72e7c5ec307c8f
Johan Geluk <jhgeluk@gmail.com>
2017-02-16 21:35:41 +0100

    Update before_build.sh

--- before_build.sh
+++ before_build.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+git submodule update --init
 echo "namespace BaggyBot{public sealed partial class Bot{public static string Version =>\""`git describe --tags`"\";}}" > ./baggybot/src/Version.cs
