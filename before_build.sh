03fb88f46886cf9ba7df0265073f114ae015123a
Johan Geluk <jhgeluk@gmail.com>
2017-02-18 14:40:07 +0100

    Update before_build.sh

--- before_build.sh
+++ before_build.sh
@@ -1,7 +1,2 @@
 #!/bin/bash
-
-cd SlackAPI
-nuget restore
-cd ..
-
 echo "namespace BaggyBot{public sealed partial class Bot{public static string Version =>\""`git describe --tags`"\";}}" > ./baggybot/src/Version.cs
