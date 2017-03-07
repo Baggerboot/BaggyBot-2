5a59a02dd7f4522c1afad68fb2519051667d4306
Baggykiin <baggykiin@geluk.io>
2017-03-07 18:26:14 +0100

    Add exception command

--- baggybot/src/Commands/Exception.cs
+++ baggybot/src/Commands/Exception.cs
@@ -1,7 +1,19 @@
-﻿namespace BaggyBot.Commands
-{
-	public class Exception
-	{
+﻿using System;
+using BaggyBot.Monitoring;
 
+namespace BaggyBot.Commands
+{
+	internal class CustomException : Command
+	{
+		public override PermissionLevel Permissions => PermissionLevel.BotOperator;
+		public override string Name => "exception";
+		public override string Usage => "";
+		public override string Description => "Throw a user-generated exception.";
+
+		public override void Use(CommandArgs command)
+		{
+			Logger.Log(this, "Throwing user-generated exception", LogLevel.Warning);
+			throw new Exception("User-requested exception");
+		}
 	}
 }
