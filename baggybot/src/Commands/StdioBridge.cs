68c9313fccda51aa0a421c7fdfeb33d46bd9d719
Baggykiin <baggykiin@geluk.io>
2017-03-17 14:38:07 +0100

    Add command groupign

--- baggybot/src/Commands/StdioBridge.cs
+++ baggybot/src/Commands/StdioBridge.cs
@@ -1,6 +1,10 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
+using BaggyBot.Formatting;
 using BaggyBot.MessagingInterface;
 
 namespace BaggyBot.Commands
@@ -8,7 +12,9 @@ namespace BaggyBot.Commands
 	internal abstract class StdioBridge : Command
 	{
 		public override PermissionLevel Permissions => PermissionLevel.BotOperator;
-		protected readonly Dictionary<ChatChannel, Process> Processes = new Dictionary<ChatChannel,Process>();
+		protected readonly Dictionary<ChatChannel, Process> Processes = new Dictionary<ChatChannel, Process>();
+
+		protected TimeSpan ReadDelay = TimeSpan.FromMilliseconds(200);
 
 		private string command, arguments;
 
@@ -24,6 +30,17 @@ namespace BaggyBot.Commands
 			{
 				Spawn(channel);
 			}
+		}
+
+		protected void Reset(ChatChannel channel)
+		{
+			if (Processes.ContainsKey(channel))
+			{
+				var proc = Processes[channel];
+				proc.Kill();
+				proc.Dispose();
+			}
+			Spawn(channel);
 		}
 
 		private void Spawn(ChatChannel channel)
@@ -45,27 +62,135 @@ namespace BaggyBot.Commands
 
 			Task.Run(() =>
 			{
+				var lastAddition = DateTime.Now;
+				var messagePool = new List<string>();
+
 				while (!proc.HasExited && !proc.StandardOutput.EndOfStream)
 				{
 					var line = proc.StandardOutput.ReadLine();
-					Client.SendMessage(channel, "--> " + line);
+					var readTime = DateTime.Now;
+
+					if (!Client.Capabilities.AllowsMultilineMessages)
+					{
+						Client.SendMessage(channel, $"{Frm.M}{line}{Frm.M}");
+						return;
+					}
+
+					lock (messagePool)
+					{
+						messagePool.Add(line);
+						lastAddition = readTime;
+					}
+
+					Task.Run(() =>
+					{
+						Task.Delay(ReadDelay).Wait();
+						lock (messagePool)
+						{
+							if (lastAddition == readTime)
+							{
+								// No new messages were added recently, so send all collected lines to the client.
+								var lines = string.Join("\n", messagePool);
+								Client.SendMessage(channel, $"{Frm.MMultiline}{lines}{Frm.MMultiline}");
+								messagePool.Clear();
+							}
+						}
+					});
 				}
 			});
 
 			Task.Run(() =>
 			{
+				var lastAddition = DateTime.Now;
+				var messagePool = new List<string>();
+
 				while (!proc.HasExited && !proc.StandardError.EndOfStream)
 				{
 					var line = proc.StandardError.ReadLine();
-					Client.SendMessage(channel, "!!> " + line);
+					var readTime = DateTime.Now;
+
+					if (!Client.Capabilities.AllowsMultilineMessages)
+					{
+						Client.SendMessage(channel, $"{Frm.M}{line}{Frm.M}");
+						return;
+					}
+
+					lock (messagePool)
+					{
+						messagePool.Add(line);
+						lastAddition = readTime;
+					}
+
+					Task.Run(() =>
+					{
+						Task.Delay(ReadDelay);
+						lock (messagePool)
+						{
+							if (lastAddition == readTime)
+							{
+								// No new messages were added recently, so send all collected lines to the client.
+								var lines = string.Join("\n", messagePool.Select(m => $"!!{m}"));
+								Client.SendMessage(channel, $"{Frm.MMultiline}{lines}{Frm.MMultiline}");
+								messagePool.Clear();
+							}
+						}
+					});
 				}
 			});
-
 		}
 
 		protected void Write(ChatChannel channel, string data)
 		{
 			Processes[channel].StandardInput.WriteLine(data);
+		}
+
+		protected void SendKey(ChatChannel channel, string key)
+		{
+			var seq = GetEscapeSequence(key);
+			Write(channel, seq);
+		}
+
+		private string GetEscapeSequence(string key)
+		{
+			switch (key)
+			{
+				case "^@":
+				case "\\0":
+					return EscapeSequence.Null;
+				case "^C":
+					return EscapeSequence.EndOfText;
+				case "^D":
+					return EscapeSequence.EndOfTransmission;
+				case "^H":
+					return EscapeSequence.Backspace;
+				case "^J":
+				case "\\n":
+					return EscapeSequence.LineFeed;
+				case "^M":
+				case "\\r":
+					return EscapeSequence.CarriageReturn;
+				case "^Q":
+					return EscapeSequence.DeviceControl1;
+				case "^S":
+					return EscapeSequence.DeviceControl3;
+				case "^[":
+				case "\\e":
+					return EscapeSequence.Escape;
+			}
+			throw new ArgumentException("Invalid escape sequence");
+		}
+
+		protected static class EscapeSequence
+		{
+			public const string Null = "\0";
+			public const string EndOfText = "\x3";
+			public const string EndOfTransmission = "\x4";
+			public const string Backspace = "\x8";
+			public const string LineFeed = "\n";
+			public const string CarriageReturn = "\r";
+			public const string DeviceControl1 = "\x11";
+			public const string DeviceControl3 = "\x13";
+			public const string Escape = "\x1b";
 		}
 	}
 }
