ece3230d70cff6da9b414424db2e86cdf0b08791
Johan Geluk <johan@ffwd.digital>
2017-06-13 22:00:24 +0200

    Add Discord message formatter

---
+++ baggybot/src/Plugins/Internal/Discord/DiscordMessageFormatter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using BaggyBot.Formatting;
+using BaggyBot.MessagingInterface;
+
+namespace BaggyBot.Plugins.Internal.Discord
+{
+	class DiscordMessageFormatter : MessageFormatter
+	{
+		protected override string GetFormattingString(FormattingType type, FormattingPosition position)
+		{
+			switch (type)
+			{
+				case FormattingType.Italic:
+					return "_";
+				case FormattingType.Bold:
+					return "*";
+				case FormattingType.Underline:
+					return "";
+				case FormattingType.Strikethrough:
+					return "~";
+				case FormattingType.Quote:
+					return ">";
+				case FormattingType.Monospace:
+					return "`";
+				case FormattingType.MonospaceBlock:
+					return "```";
+				default:
+					throw new ArgumentOutOfRangeException(nameof(type), type, null);
+			}
+		}
+
+		public override ChatMessage ProcessIncomingMessage(ChatMessage message)
+		{
+			return message;
+		}
+	}
+}
